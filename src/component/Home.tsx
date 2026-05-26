44c3ed7d49808e97a2a7defd68b56c1ce8fe8cab
jsilver01 <kje2066@naver.com>
2026-05-26 16:40:42 +0900

    feat: 네비게이션 화면 구성완료

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,7 +1,11 @@
+import "./Home.css";
+import NavigationBar from "./NavigationBar";
+
 const Home = () => {
   return (
-    <div>
+    <div className="home_container">
       <h1>HOME 화면입니다.</h1>
+      <NavigationBar />
     </div>
   );
 };
---
+++ src/component/NavigationBar.tsx
@@ -0,0 +1,37 @@
+import "./NavigationBar.css";
+import HomeIcon from "../img/home-page.png";
+import CultureIcon from "../img/teamwork.png";
+
+const NavigationBar = () => {
+  return (
+    <div className="bottom_nav_container">
+      <div className="bottom_nav">
+        <div className="nav_section">
+          <div className="nav_item">
+            <div className="icon">
+              <img className="icon_img" src={HomeIcon} />
+            </div>
+            <p>Home</p>
+          </div>
+        </div>
+
+        <div className="nav_section"></div>
+
+        <div className="nav_section">
+          <div className="nav_item">
+            <div className="icon">
+              <img className="icon_img" src={CultureIcon} />
+            </div>
+            <p>H.Culture</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="floating_button">
+        <p>MY.H</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
