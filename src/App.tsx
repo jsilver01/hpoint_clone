f4d111ce8628dae5f6c4c64d409746a9821a95b1
jungeun <kje2066@naver.com>
2026-05-26 11:43:24 +0900

    스플래시 화면

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import SplashScreen from "./component/SplashScreen";
+import Home from "./component/Home";
 
 function App() {
-  return <div></div>;
+  const [isSplash, setIsSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSplash(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <div>{isSplash ? <SplashScreen /> : <Home />}</div>;
 }
 
 export default App;
---
+++ src/component/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SplashScreen = () => {
+  return (
+    <div>
+      <h1>myApp</h1>
+    </div>
+  );
+};
+
+export default SplashScreen;
