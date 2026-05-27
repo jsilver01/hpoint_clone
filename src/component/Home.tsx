a2887db1bbdd384e9c34a1b42ee28e3fdc2b1489
jsilver01 <kje2066@naver.com>
2026-05-27 17:28:35 +0900

    WIP: home header 컴포넌트 레이아웃 구현

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,6 +1,9 @@
 import "./Home.css";
 import NavigationBar from "./NavigationBar";
 import CodeIcon from "../img/code.png";
+import PointIcon from "../img/dollar-circle.png";
+import Notification from "../img/notification.png";
+import Hamburger from "../img/Hamburger_MD.png";
 
 const Home = () => {
   return (
@@ -8,9 +11,16 @@ const Home = () => {
       <div className="main_wrapper">
         <div className="ad_background_section">
           <div className="header_container">
-            <div className="left_section"></div>
+            <div className="left_section">
+              <img alt="code_img" src={CodeIcon} />
+              <p>로그인을 해주세요</p>
+            </div>
 
-            <div className="right_section"></div>
+            <div className="right_section">
+              <img alt="point_img" src={PointIcon} />
+              <img alt="notification_img" src={Notification} />
+              <img alt="hamburger" src={Hamburger} />
+            </div>
           </div>
         </div>
 
